048f2acf5a35e3f6b3454928b685305ad9a99839
Stefan Einarsson <stefane81@gmail.com>
2019-02-25 17:03:43 +0100

    Deploy to heroku

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const app = express();
+
+app.get("/", (request, response) => {
+  response.send({ hi: "there" });
+});
+
+// dynamically allocate port
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT);
