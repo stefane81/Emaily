8876336166f2145b1a14a233a71bfd9e8fe078b5
Stefan Einarsson <stefane81@gmail.com>
2019-02-27 20:31:23 +0100

    Add User collection to mongodb record only once

--- services/passport.js
+++ services/passport.js
@@ -1,7 +1,9 @@
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const mongoose = require('mongoose');
 const keys = require('../config/keys');
 
+const User = mongoose.model('users');
 // passport
 passport.use(
 	new GoogleStrategy(
@@ -11,9 +13,13 @@ passport.use(
 			callbackURL: '/auth/google/callback'
 		},
 		(accessToken, refreshToken, profile, done) => {
-			console.log('accessToken: ', accessToken);
-			console.log('refreshToken: ', refreshToken);
-			console.log('profile: ', profile);
+			User.findOne({ googleID: profile.id }).then(existingUser => {
+				if (existingUser) {
+					// we already have a record of this user
+				} else {
+					new User({ googleID: profile.id }).save();
+				}
+			});
 		}
 	)
 );
