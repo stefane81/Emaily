bf75f1644b3fd92bd1b2f3495acc4c23f06ff64d
Stefan Einarsson <stefane81@gmail.com>
2019-02-27 18:50:51 +0100

    Adding mongoDB

--- index.js
+++ index.js
@@ -1,35 +1,14 @@
 const express = require('express');
-const passport = require('passport');
-const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const mongoose = require('mongoose');
 const keys = require('./config/keys');
+require('./services/passport');
+require('./models/User');
+
+mongoose.connect(keys.mongoURI);
+const authRoutes = require('./routes/authRoutes');
 
 const app = express();
-
-// passport
-
-passport.use(
-	new GoogleStrategy(
-		{
-			clientID: keys.googleClientID,
-			clientSecret: keys.googleClientSecret,
-			callbackURL: '/auth/google/callback'
-		},
-		(accessToken, refreshToken, profile, done) => {
-			console.log('accessToken: ', accessToken);
-			console.log('refreshToken: ', refreshToken);
-			console.log('profile: ', profile);
-		}
-	)
-);
-
-app.get(
-	'/auth/google',
-	passport.authenticate('google', {
-		scope: ['profile', 'email']
-	})
-);
-
-app.get('/auth/google/callback', passport.authenticate('google'));
+authRoutes(app);
 
 // dynamically allocate port
 const PORT = process.env.PORT || 5000;
