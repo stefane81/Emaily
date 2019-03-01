2c249d3be8c26bf032adbb59dd330e5333df3923
Stefan Einarsson <stefane81@gmail.com>
2019-03-01 17:17:00 +0100

    Google authentication correctly logging in

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const cookieSession = require('cookie-session');
+const passport = require('passport');
 const keys = require('./config/keys');
 require('./models/User');
 require('./services/passport');
@@ -8,6 +10,17 @@ mongoose.connect(keys.mongoURI);
 const authRoutes = require('./routes/authRoutes');
 
 const app = express();
+
+app.use(
+	cookieSession({
+		maxAge: 30 * 24 * 60 * 60 * 100,
+		keys: [keys.cookieKey]
+	})
+);
+
+app.use(passport.initialize());
+app.use(passport.session());
+
 authRoutes(app);
 
 // dynamically allocate port
--- services/passport.js
+++ services/passport.js
@@ -1,10 +1,27 @@
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
 const mongoose = require('mongoose');
+
 const keys = require('../config/keys');
 
 const User = mongoose.model('users');
 // passport
+
+passport.serializeUser((user, done) => {
+	// user.id is a unique identifier to the 'row-id'
+	// used because the user might have another authentication method than googleID (profile.id)
+	done(null, user.id);
+});
+
+// this id is from user.id
+// turn id into mongo id instance
+passport.deserializeUser((id, done) => {
+	// find user by mongodb id and pass user through passport
+	User.findById(id).then(user => {
+		done(null, user);
+	});
+});
+
 passport.use(
 	new GoogleStrategy(
 		{
@@ -16,8 +33,11 @@ passport.use(
 			User.findOne({ googleID: profile.id }).then(existingUser => {
 				if (existingUser) {
 					// we already have a record of this user
+					done(null, existingUser);
 				} else {
-					new User({ googleID: profile.id }).save();
+					new User({ googleID: profile.id })
+						.save()
+						.then(user => done(null, user));
 				}
 			});
 		}
