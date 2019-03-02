89789b2de8bd782b17bf1dc2b21c6e98778c7fb6
Stefan Einarsson <stefane81@gmail.com>
2019-03-02 17:14:06 +0100

    Adding production keys

---
+++ config/keys.js
@@ -0,0 +1,8 @@
+// keys js
+if (process.env.NODE_ENV === 'production') {
+	// we are in production
+	module.exports = require('./prod');
+} else {
+	// we are in dev mode
+	module.exports = require('./dev');
+}
